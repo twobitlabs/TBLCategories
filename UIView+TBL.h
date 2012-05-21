a14e7654a96e0e222911dbcdf9a8141e824ea2dd
Susan Detwiler <susan@twobitlabs.com>
2012-05-21 15:22:53 -0700

    Added category on UIView

---
+++ UIView+TBL.h
@@ -0,0 +1,15 @@
+//
+//  UIView+TBL.h
+//  TeamStream
+//
+//  Created by Susan Detwiler on 5/21/12.
+//  Copyright (c) 2012 Bleacher Report. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (TBL)
+
+-(void)roundCorners:(UIRectCorner)corners;
+
+@end
---
+++ UIView+TBL.m
@@ -0,0 +1,25 @@
+//
+//  UIView+TBL.m
+//  TeamStream
+//
+//  Created by Susan Detwiler on 5/21/12.
+//  Copyright (c) 2012 Bleacher Report. All rights reserved.
+//
+
+#import "UIView+TBL.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation UIView (TBL)
+
+-(void)roundCorners:(UIRectCorner)corners{
+    CAShapeLayer *maskLayer = [CAShapeLayer layer];
+    UIBezierPath *roundedPath = [UIBezierPath bezierPathWithRoundedRect:self.bounds
+                                                      byRoundingCorners:corners
+                                                            cornerRadii:CGSizeMake(8,8)];
+    maskLayer.fillColor = [[UIColor whiteColor] CGColor];
+    maskLayer.backgroundColor = [[UIColor clearColor] CGColor];
+    maskLayer.path = [roundedPath CGPath];
+    self.layer.mask = maskLayer;
+}
+
+@end
