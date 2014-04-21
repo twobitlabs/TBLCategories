38a2107e0937d479c21e6d332692f1a685050aac
Dave Roy <droy@twobitlabs.com>
2014-04-21 10:55:08 -0700

    Warn of attempt to load uncached image from xcassets.

--- UIImage+TBL.m
+++ UIImage+TBL.m
@@ -17,6 +17,16 @@
     if (path != nil) {
         image = [UIImage imageWithContentsOfFile:path];
     }
+#ifdef DEBUG
+    // Images in xcassets will not be found by poking around in the mainBundle.
+    // shouldCache:NO does not work for images in xcassets and will always
+    // return nil.
+    if (!image && !cache) {
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:[NSString stringWithFormat:@"Image not found.  If the image is in xcassets you must pass shouldCache:YES or use the default [UIImage imageNamed:] method."]
+                                     userInfo:nil];
+    }
+#endif
     return image;
 }
 
