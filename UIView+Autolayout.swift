5e596c6913d7b37b5a48875f96f1c8e10895e5de
Christopher Pickslay <cpickslay@twobitlabs.com>
2015-01-13 13:51:22 -0800

    added methods for centering child views to UIView+Autolayout.swift

--- UIView+Autolayout.swift
+++ UIView+Autolayout.swift
@@ -10,5 +10,34 @@ extension UIView {
             addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(constraintDescription, views: views, options:options, metrics:metrics))
         }
     }
+    
+    func centerChildHorizontally(childView: UIView) {
+        childView.setTranslatesAutoresizingMaskIntoConstraints(false);
+        let centerHorizontally = NSLayoutConstraint(item:childView,
+            attribute:.CenterX,
+            relatedBy:.Equal,
+            toItem:self,
+            attribute:.CenterX,
+            multiplier:1,
+            constant:0);
+        addConstraint(centerHorizontally);
+    }
+    
+    func centerChildVertically(childView: UIView) {
+        childView.setTranslatesAutoresizingMaskIntoConstraints(false);
+        let centerVertically = NSLayoutConstraint(item:childView,
+            attribute:.CenterY,
+            relatedBy:.Equal,
+            toItem:self,
+            attribute:.CenterY,
+            multiplier:1,
+            constant:0);
+        addConstraint(centerVertically);
+    }
 
+    func centerChild(childView: UIView) {
+        centerChildHorizontally(childView)
+        centerChildVertically(childView)
+    }
+    
 }
