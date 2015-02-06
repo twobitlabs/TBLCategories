22394c531f211a14abba4d2d297e5c02082bb07f
Christopher Pickslay <cpickslay@twobitlabs.com>
2015-02-05 22:54:45 -0800

    added methods for creating sizing and positioning constraints

--- UIView+Autolayout.swift
+++ UIView+Autolayout.swift
@@ -11,27 +11,35 @@ extension UIView {
         }
     }
     
+    // MARK: - centering
+    
     func centerChildHorizontally(childView: UIView) {
         childView.setTranslatesAutoresizingMaskIntoConstraints(false);
-        let centerHorizontally = NSLayoutConstraint(item:childView,
-            attribute:.CenterX,
-            relatedBy:.Equal,
-            toItem:self,
-            attribute:.CenterX,
-            multiplier:1,
-            constant:0);
+        let centerHorizontally = NSLayoutConstraint(item: childView,
+            attribute: .CenterX,
+            relatedBy: .Equal,
+            toItem: self,
+            attribute: .CenterX,
+            multiplier: 1,
+            constant: 0);
         addConstraint(centerHorizontally);
+    }
+    
+    func centerChildrenHorizontally(childViews: [UIView]) {
+        for view in childViews {
+            centerChildHorizontally(view)
+        }
     }
     
     func centerChildVertically(childView: UIView) {
         childView.setTranslatesAutoresizingMaskIntoConstraints(false);
-        let centerVertically = NSLayoutConstraint(item:childView,
-            attribute:.CenterY,
-            relatedBy:.Equal,
-            toItem:self,
-            attribute:.CenterY,
-            multiplier:1,
-            constant:0);
+        let centerVertically = NSLayoutConstraint(item: childView,
+            attribute: .CenterY,
+            relatedBy: .Equal,
+            toItem: self,
+            attribute: .CenterY,
+            multiplier: 1,
+            constant: 0);
         addConstraint(centerVertically);
     }
 
@@ -40,4 +48,86 @@ extension UIView {
         centerChildVertically(childView)
     }
     
+    // MARK: - positioning
+    
+    func offsetTopFromParent(offset: CGFloat) {
+        let superview = self.superview!
+        setTranslatesAutoresizingMaskIntoConstraints(false)
+        let topOffset = NSLayoutConstraint(item: self,
+            attribute: .Top,
+            relatedBy: .Equal,
+            toItem: superview,
+            attribute: .Top,
+            multiplier: 1,
+            constant: offset);
+        superview.addConstraint(topOffset);
+    }
+    
+    func placeBelow(sibling: UIView, by offset: CGFloat) {
+        let superview = self.superview!
+        if (superview != sibling.superview!) {
+            return
+        }
+        setTranslatesAutoresizingMaskIntoConstraints(false)
+        sibling.setTranslatesAutoresizingMaskIntoConstraints(false)
+        let topOffset = NSLayoutConstraint(item: self,
+            attribute: .Top,
+            relatedBy: .Equal,
+            toItem: sibling,
+            attribute: .Bottom,
+            multiplier: 1,
+            constant: offset);
+        superview.addConstraint(topOffset);
+    }
+    
+    func pinToTop() {
+        let superview = self.superview!
+        setTranslatesAutoresizingMaskIntoConstraints(false)
+        let topOffset = NSLayoutConstraint(item: self,
+            attribute: .Top,
+            relatedBy: .Equal,
+            toItem: superview,
+            attribute: .Top,
+            multiplier: 1,
+            constant: 0);
+        superview.addConstraint(topOffset);
+    }
+    
+    func pinToBottom() {
+        let superview = self.superview!
+        setTranslatesAutoresizingMaskIntoConstraints(false)
+        let topOffset = NSLayoutConstraint(item: self,
+            attribute: .Bottom,
+            relatedBy: .Equal,
+            toItem: superview,
+            attribute: .Bottom,
+            multiplier: 1,
+            constant: 0);
+        superview.addConstraint(topOffset);
+    }
+    
+    // MARK: - sizing
+
+    func setWidthConstraint(size: CGFloat) {
+        setSizeConstraint(size, dimension: .Width)
+    }
+    
+    func setHeightConstraint(size: CGFloat) {
+        setSizeConstraint(size, dimension: .Height)
+    }
+    
+    private func setSizeConstraint(size: CGFloat, dimension: NSLayoutAttribute) {
+        let superview = self.superview!
+        setTranslatesAutoresizingMaskIntoConstraints(false)
+        let widthConstraint = NSLayoutConstraint(item: self,
+            attribute: dimension,
+            relatedBy: .Equal,
+            toItem: nil,
+            attribute: .NotAnAttribute,
+            multiplier: 1,
+            constant: size)
+        superview.addConstraint(widthConstraint);
+    }
+    
+
 }
