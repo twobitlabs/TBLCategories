5576c81487c21a602d6f5b54544daac2dd522ad7
Christopher Pickslay <cpickslay@twobitlabs.com>
2015-02-26 16:53:21 -0800

    added method to get an image in the given color, suitable for background images

--- UIColor+TBL.swift
+++ UIColor+TBL.swift
@@ -24,5 +24,16 @@ extension UIColor {
         
         return nil
     }
+
+    func asImage() -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context: CGContext = UIGraphicsGetCurrentContext()
+        CGContextSetFillColorWithColor(context, CGColor)
+        CGContextFillRect(context, rect)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
         
 }
