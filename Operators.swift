50ea7b904b0668cf36ba5493a3c8ded7adc5e772
Christopher Pickslay <cpickslay@twobitlabs.com>
2015-03-20 17:33:04 -0700

    changed documentation to standard format

--- Operators.swift
+++ Operators.swift
@@ -2,8 +2,10 @@
 
 infix operator ??= { associativity left precedence 90 }
 
-// If the right-hand side is NOT nil, the left-hand side is set to the value of the right-hand side.
-// If the right-hand side is nil, no change is made to the left-hand side.
+/**
+    If the right-hand side is NOT nil, the left-hand side is set to the value of the right-hand side.
+    If the right-hand side is nil, no change is made to the left-hand side.
+*/
 func ??= <T>(inout lhs: T, rhs: T?) {
     if let rhs = rhs {
         lhs = rhs
