e3837b924367ff68219448ed6f04c46a14492699
Christopher Pickslay <cpickslay@twobitlabs.com>
2015-04-02 16:37:48 -0700

    do not return nil for images in asset catalog if shouldCache == true

--- UIImage+TBL.m
+++ UIImage+TBL.m
@@ -8,17 +8,19 @@
     NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:type];
     if (path != nil) {
         image = [UIImage imageWithContentsOfFile:path];
-    }
 #ifdef DEBUG
-    // Images in xcassets will not be found by poking around in the mainBundle.
-    // shouldCache:NO does not work for images in xcassets and will always
-    // return nil.
-    if (!image && !cache) {
-        @throw [NSException exceptionWithName:NSInternalInconsistencyException
-                                       reason:[NSString stringWithFormat:@"Image not found.  If the image is in xcassets you must pass shouldCache:YES or use the default [UIImage imageNamed:] method."]
-                                     userInfo:nil];
-    }
+        // Images in xcassets will not be found by poking around in the mainBundle.
+        // shouldCache:NO does not work for images in xcassets and will always
+        // return nil.
+        if (!image) {
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:[NSString stringWithFormat:@"Image not found.  If the image is in xcassets you must pass shouldCache:YES or use the default [UIImage imageNamed:] method."]
+                                         userInfo:nil];
+        }
 #endif
+    } else {
+        image = [self imageNamed:name];
+    }
     return image;
 }
 
