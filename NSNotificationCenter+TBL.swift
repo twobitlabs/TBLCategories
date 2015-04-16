59a5fec5890190b0a9fe176b4a850fc1eee70a1c
Dave Roy <droy@twobitlabs.com>
2015-04-16 09:06:18 -0700

    Removed due to leak potential with methods

--- NSNotificationCenter+TBL.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension NSNotificationCenter {
-    
-    class func addObserver(#callback: (NSNotification!) -> (), name: String? = nil, object: AnyObject? = nil, queue: NSOperationQueue? = nil) {
-        defaultCenter().addObserverForName(name, object: object, queue: nil, usingBlock: callback)
-    }
-    
-}
