b7ba8fc864942830dd0156f2867a86578032a4bb
Todd Huss <thuss@gabrito.com>
2015-06-10 14:33:44 -0700

    Added category on UIImage to allow recoloring of UIImage's

---
+++ Swift Extensions/UIImage+TBL.swift
@@ -0,0 +1,20 @@
+extension UIImage {
+    func imageWithColor(color: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+
+        let context = UIGraphicsGetCurrentContext() as CGContextRef
+        CGContextTranslateCTM(context, 0, self.size.height)
+        CGContextScaleCTM(context, 1.0, -1.0);
+        CGContextSetBlendMode(context, kCGBlendModeNormal)
+
+        let rect = CGRectMake(0, 0, self.size.width, self.size.height) as CGRect
+        CGContextClipToMask(context, rect, self.CGImage)
+        color.setFill()
+        CGContextFillRect(context, rect)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+}
