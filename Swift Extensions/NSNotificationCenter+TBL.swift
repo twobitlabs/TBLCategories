383922fcb16613230e794667246ce300528d7310
Todd Huss <thuss@twobitlabs.com>
2015-06-11 14:31:15 -0700

    Removed NSNotificationCenter category

--- Swift Extensions/NSNotificationCenter+TBL.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public extension NSNotificationCenter {
-    
-    public class func addObserver(name: String? = nil, object: AnyObject? = nil, queue: NSOperationQueue? = nil, callback: (NSNotification!) -> ()) {
-        defaultCenter().addObserverForName(name, object: object, queue: nil, usingBlock: callback)
-    }
-    
-}
