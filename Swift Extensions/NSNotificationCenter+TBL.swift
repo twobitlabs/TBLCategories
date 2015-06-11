3d82f9db67c0bfc25df9492d47d33a279b1e355e
Todd Huss <thuss@gabrito.com>
2015-06-11 14:26:49 -0700

    Updated NSNotificationCenter category method signature to fix swift warning

--- Swift Extensions/NSNotificationCenter+TBL.swift
+++ Swift Extensions/NSNotificationCenter+TBL.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public extension NSNotificationCenter {
     
-    public class func addObserver(#callback: (NSNotification!) -> (), name: String? = nil, object: AnyObject? = nil, queue: NSOperationQueue? = nil) {
+    public class func addObserver(name: String? = nil, object: AnyObject? = nil, queue: NSOperationQueue? = nil, callback: (NSNotification!) -> ()) {
         defaultCenter().addObserverForName(name, object: object, queue: nil, usingBlock: callback)
     }
     
