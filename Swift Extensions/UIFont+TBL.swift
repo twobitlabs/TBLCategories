369b293f25545efcba53a7584a1396e0f9fadf75
Christopher Pickslay <cpickslay@twobitlabs.com>
2015-06-19 12:31:36 -0700

    added UIFont extension to get preferred font size on a given font

---
+++ Swift Extensions/UIFont+TBL.swift
@@ -0,0 +1,37 @@
+
+import UIKit
+
+extension UIFont {
+
+    public enum UIFontTextStyle {
+        case Headline
+        case Subheadline
+        case Body
+        case Footnote
+        case Caption1
+        case Caption2
+
+        private var style: String {
+            switch self {
+            case Headline:
+                return UIFontTextStyleHeadline
+            case Subheadline:
+                return UIFontTextStyleSubheadline
+            case Body:
+                return UIFontTextStyleBody
+            case Footnote:
+                return UIFontTextStyleFootnote
+            case Caption1:
+                return UIFontTextStyleCaption1
+            case Caption2:
+                return UIFontTextStyleCaption2
+            }
+        }
+    }
+
+    public func preferredFontForTextStyle(style: UIFontTextStyle) -> UIFont {
+        let descriptor = UIFontDescriptor.preferredFontDescriptorWithTextStyle(style.style)
+        return self.fontWithSize(descriptor.pointSize)
+    }
+    
+}
