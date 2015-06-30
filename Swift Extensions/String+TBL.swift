00e05d802a42b803880b29aeea903fd44a18fe49
Dave Roy <droy@twobitlabs.com>
2015-06-30 11:58:36 -0700

    Method to capitalize only first letter

--- Swift Extensions/String+TBL.swift
+++ Swift Extensions/String+TBL.swift
@@ -12,6 +12,22 @@ public extension String {
         }
     }
 
+    /**
+    Capitalize the first letter as opposed to .capitalizedString which lowercases
+    the string and then capitalizes every word in it.
+
+    :returns: Same string with the first letter uppercased
+    */
+    func capitalizeFirstLetter() -> String {
+        if startIndex == endIndex {
+            return self
+        }
+        let rest = advance(startIndex,1)..<endIndex
+        let capitalized = self[startIndex...startIndex].uppercaseString + self[rest]
+
+        return capitalized
+    }
+
     public static func localized(key: String, table: String) -> String {
         return NSLocalizedString(key, tableName: table, comment: "")
     }
