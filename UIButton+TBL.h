cdab0a422fada2922b7d145245c4dafdc3689952
Christopher Pickslay <cpickslay@twobitlabs.com>
2015-07-06 15:38:56 -0700

    removed potentially unsafe category in favor of doing KVO on the highlighted state

--- UIButton+TBL.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface UIButton (TBL)
-
--(void)setBackgroundColor:(UIColor *)color forState:(UIControlState)state;
--(UIColor*)backgroundColorForState:(UIControlState)state;
-
-@end
