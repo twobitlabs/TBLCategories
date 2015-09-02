4f80a6e3888236ca1eeda054159e6456cd35812b
Dave Roy <droy@twobitlabs.com>
2015-09-02 14:47:52 -0700

    Update placeToLeft* calls to be consistent with placeToRight*

--- Swift Extensions/UIView+Autolayout.swift
+++ Swift Extensions/UIView+Autolayout.swift
@@ -166,7 +166,7 @@ public extension UIView {
         return rightOffset
     }
 
-    public func placeToLeftOf(sibling: UIView, by offset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    public func placeToLeftOf(sibling: UIView, by offset: CGFloat = 0, identifier: String? = nil) -> NSLayoutConstraint {
         let superview = self.superview!
         if (superview != sibling.superview!) {
             fatalError("views do not share the same superview")
@@ -176,6 +176,30 @@ public extension UIView {
         let leftOffset = NSLayoutConstraint(item: self,
             attribute: .Right,
             relatedBy: .Equal,
+            toItem: sibling,
+            attribute: .Left,
+            multiplier: 1,
+            constant: -offset)
+        leftOffset.active = true
+        leftOffset.identifier = identifier
+        return leftOffset
+    }
+
+    /**
+    Add a `NSLayoutConstraint` that positions the caller at least `offset` points to the left of `sibling`
+
+    :returns: The constraint that was added to the caller's superview
+    */
+    public func placeToLeftOf(sibling: UIView, byAtLeast offset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+        let superview = self.superview!
+        if (superview != sibling.superview!) {
+            fatalError("views do not share the same superview")
+        }
+        setTranslatesAutoresizingMaskIntoConstraints(false)
+        sibling.setTranslatesAutoresizingMaskIntoConstraints(false)
+        let leftOffset = NSLayoutConstraint(item: self,
+            attribute: .Right,
+            relatedBy: .LessThanOrEqual,
             toItem: sibling,
             attribute: .Left,
             multiplier: 1,
