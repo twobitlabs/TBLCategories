ce321715d4bcf066fa4b8adcbfd64991ce48801b
Christopher Pickslay <cpickslay@twobitlabs.com>
2015-11-20 11:48:11 -0800

    fixed fromhexstring always returning nil

--- Swift Extensions/UIColor+TBL.swift
+++ Swift Extensions/UIColor+TBL.swift
@@ -2,27 +2,25 @@ import UIKit
 
 public extension UIColor {
 
+    /**
+        Parse a string for a hex color value. Currently does not support 8-character strings with alpha components
+     */
     public convenience init?(fromHexString hexString: String) {
         let colorHexString = hexString.stringByReplacingOccurrencesOfString("#", withString: "", options: NSStringCompareOptions(), range: nil)
         let colorScanner = NSScanner(string: colorHexString)
+        let maxColorValue = UInt32(pow(Float(16), Float(6)))
 
         var value:UInt32 = 0
 
         let characters = colorHexString.characters.count
-        if (characters == 6) {
-            // no alpha supported
-            if (colorScanner.scanHexInt(&value)) {
-                let maxColorValue = UInt32(pow(Float(16), Float(6)))
-                if (value < maxColorValue) {
-                    let red = (CGFloat)((value & 0xFF0000) >> 16)/255.0
-                    let green = (CGFloat)((value & 0xFF00) >> 8)/255.0
-                    let blue = (CGFloat)(value & 0xFF)/255.0
-                    self.init(red: red, green: green, blue: blue, alpha: 1.0)
-                }
-            }
+        guard characters == 6 && colorScanner.scanHexInt(&value) &&  value < maxColorValue else {
+            return nil
         }
 
-        return nil
+        let red = (CGFloat)((value & 0xFF0000) >> 16)/255.0
+        let green = (CGFloat)((value & 0xFF00) >> 8)/255.0
+        let blue = (CGFloat)(value & 0xFF)/255.0
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
     }
 
     public func asImage() -> UIImage {
