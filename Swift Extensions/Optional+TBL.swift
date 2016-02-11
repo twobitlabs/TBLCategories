8ca38b6c3c0fa4dc68be723ef8e1b038359c0f4d
Christopher Pickslay <cpickslay@twobitlabs.com>
2016-02-10 16:15:03 -0800

    added isEmpty extension to optional strings and dictionaries

--- Swift Extensions/Optional+TBL.swift
+++ Swift Extensions/Optional+TBL.swift
@@ -11,3 +11,23 @@ extension Optional {
         }
     }
 }
+
+//protocol CanBeEmpty {
+//    var isEmpty: Bool { get }
+//}
+//
+//extension String: CanBeEmpty {}
+//extension Dictionary: CanBeEmpty {}
+//
+//extension Optional where Wrapped : CanBeEmpty {
+//    var isEmpty: Bool {
+//        get {
+//            switch self {
+//            case .None:
+//                return true
+//            case .Some(let maybeEmpty):
+//                return maybeEmpty.isEmpty
+//            }
+//        }
+//    }
+//}
