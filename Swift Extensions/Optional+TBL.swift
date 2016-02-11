68ec6ce01a462a237f424670e66ec7c1cc66e6f7
Christopher Pickslay <cpickslay@twobitlabs.com>
2016-02-11 09:34:35 -0800

    uncommented isEmpty extension

--- Swift Extensions/Optional+TBL.swift
+++ Swift Extensions/Optional+TBL.swift
@@ -12,22 +12,22 @@ extension Optional {
     }
 }
 
-//protocol CanBeEmpty {
-//    var isEmpty: Bool { get }
-//}
-//
-//extension String: CanBeEmpty {}
-//extension Dictionary: CanBeEmpty {}
-//
-//extension Optional where Wrapped : CanBeEmpty {
-//    var isEmpty: Bool {
-//        get {
-//            switch self {
-//            case .None:
-//                return true
-//            case .Some(let maybeEmpty):
-//                return maybeEmpty.isEmpty
-//            }
-//        }
-//    }
-//}
+protocol CanBeEmpty {
+    var isEmpty: Bool { get }
+}
+
+extension String: CanBeEmpty {}
+extension Dictionary: CanBeEmpty {}
+
+extension Optional where Wrapped : CanBeEmpty {
+    var isEmpty: Bool {
+        get {
+            switch self {
+            case .None:
+                return true
+            case .Some(let maybeEmpty):
+                return maybeEmpty.isEmpty
+            }
+        }
+    }
+}
