440a34f4b8be85ac99a57f9b3d53dddd115f1b55
Christopher Pickslay <cpickslay@twobitlabs.com>
2016-05-10 12:05:53 -0700

    fixed nil coalesce operator

--- Swift Extensions/Operators.swift
+++ Swift Extensions/Operators.swift
@@ -5,7 +5,7 @@ infix operator ??= { associativity left precedence 90 }
 /**
  If the left-hand side is nil, assign to the value of the right-hand side.
  */
-public func ??= <T>(inout lhs: T, rhs: T?) {
+public func ??= <T>(inout lhs: T?, rhs: T?) {
   if lhs == nil {
       lhs = rhs
   }
