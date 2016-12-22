df819b89a959ed56f88fd31f62d235c81fa60b2c
Young Fan <yfan@twobitlabs.com>
2016-12-21 21:24:01 -0800

    Update UIViewController extension to Swift 3

--- Swift Extensions/UIViewController+TBL.swift
+++ Swift Extensions/UIViewController+TBL.swift
@@ -4,19 +4,19 @@ import UIKit
 extension UIViewController {
     public func dismissAlertIfNecessary() {
         if presentedViewController is UIAlertController {
-            dismissViewControllerAnimated(false, completion: nil)
+            dismiss(animated: false, completion: nil)
         }
     }
 
     public func showAlert(title: String, message: String? = nil, actions: [UIAlertAction]? = nil)  {
-        let alert = LoggingAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let alert = LoggingAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
         if actions == nil || actions!.isEmpty {
-            alert.addAction(UIAlertAction(title: String.localized("OK"), style: UIAlertActionStyle.Default, handler: nil))
+            alert.addAction(UIAlertAction(title: String.localized("OK"), style: UIAlertActionStyle.default, handler: nil))
         } else {
             for action in actions! {
                 alert.addAction(action)
             }
         }
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
 }
