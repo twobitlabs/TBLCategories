1f15927787de98d022ffdb81da433aab1913e1de
Todd Huss <thuss@twobitlabs.com>
2017-03-30 12:46:11 -0700

    Marks autolayout results as discardable

--- Swift Extensions/UIView+Autolayout-objc-compat.swift
+++ Swift Extensions/UIView+Autolayout-objc-compat.swift
@@ -3,79 +3,79 @@ import UIKit
     Compatibility methods for our old UIView+Autolayout objc category and objc code that can't pass priority, which is a Float
 */
 public extension UIView {
-    public func addConstraintsFromDescriptions(_ constraintDescriptions: [String], views: Dictionary<String, UIView>) -> [NSLayoutConstraint] {
+    @discardableResult public func addConstraintsFromDescriptions(_ constraintDescriptions: [String], views: Dictionary<String, UIView>) -> [NSLayoutConstraint] {
         return addConstraintsFromDescriptions(constraintDescriptions, views: views, metrics: nil)
     }
 
-    public func addConstraintsFromDescriptions(_ constraintDescriptions: [String], metrics: Dictionary<String, NSNumber>? = nil, views: Dictionary<String, UIView>) -> [NSLayoutConstraint] {
+    @discardableResult public func addConstraintsFromDescriptions(_ constraintDescriptions: [String], metrics: Dictionary<String, NSNumber>? = nil, views: Dictionary<String, UIView>) -> [NSLayoutConstraint] {
         return addConstraintsFromDescriptions(constraintDescriptions, views: views, metrics: metrics)
     }
 
-    public func objc_pinToTop(identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_pinToTop(identifier: String? = nil) -> NSLayoutConstraint {
         return pinToTop(priority: nil, identifier: identifier)
     }
 
-    public func objc_pinToBottom(identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_pinToBottom(identifier: String? = nil) -> NSLayoutConstraint {
         return pinToBottom(priority: nil, identifier: identifier)
     }
 
-    public func objc_pinToLeft(identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_pinToLeft(identifier: String? = nil) -> NSLayoutConstraint {
         return pinToLeft(priority: nil, identifier: identifier)
     }
 
-    public func objc_pinToRight(identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_pinToRight(identifier: String? = nil) -> NSLayoutConstraint {
         return pinToRight(priority: nil, identifier: identifier)
     }
 
-    public func objc_insetFromParentLeft(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_insetFromParentLeft(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return insetFromParentLeft(inset, priority: nil, identifier: identifier)
     }
 
-    public func objc_insetFromParentRight(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_insetFromParentRight(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return insetFromParentRight(inset, priority: nil, identifier: identifier)
     }
 
-    public func objc_insetFromParentTop(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_insetFromParentTop(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return insetFromParentTop(inset, priority: nil, identifier: identifier)
     }
 
-    func objc_insetFromParentBottom(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult func objc_insetFromParentBottom(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return insetFromParentBottom(inset, priority: nil, identifier: identifier)
     }
 
-    func objc_insetFromParentLeftByAtLeast(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult func objc_insetFromParentLeftByAtLeast(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return insetFromParentLeftByAtLeast(inset, priority: nil, identifier: identifier)
     }
 
-    func objc_insetFromParentRightByAtLeast(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult func objc_insetFromParentRightByAtLeast(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return insetFromParentRightByAtLeast(inset, priority: nil, identifier: identifier)
     }
 
-    func objc_insetFromParentTopByAtLeast(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult func objc_insetFromParentTopByAtLeast(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return insetFromParentTopByAtLeast(inset, priority: nil, identifier: identifier)
     }
 
-    public func objc_insetFromParentBottomByAtLeast(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_insetFromParentBottomByAtLeast(_ inset: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return insetFromParentBottomByAtLeast(inset, priority: nil, identifier: identifier)
     }
 
-    public func objc_fillParentVertically(identifier: String? = nil) -> [NSLayoutConstraint] {
+    @discardableResult public func objc_fillParentVertically(identifier: String? = nil) -> [NSLayoutConstraint] {
         return fillParentVertically(priority: nil, identifier: identifier)
     }
 
-    public func objc_fillParentHorizontally(identifier: String? = nil) -> [NSLayoutConstraint] {
+    @discardableResult public func objc_fillParentHorizontally(identifier: String? = nil) -> [NSLayoutConstraint] {
         return fillParentHorizontally(priority: nil, identifier: identifier)
     }
 
-    public func objc_centerChildVertically(_ childView: UIView, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_centerChildVertically(_ childView: UIView, identifier: String? = nil) -> NSLayoutConstraint {
         return centerChildVertically(childView, priority: nil, identifier: identifier)
     }
 
-    public func objc_setHeightConstraint(_ size: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_setHeightConstraint(_ size: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return setHeightConstraint(size, priority: nil, identifier: identifier)
     }
 
-    public func objc_setWidthConstraint(_ size: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
+    @discardableResult public func objc_setWidthConstraint(_ size: CGFloat, identifier: String? = nil) -> NSLayoutConstraint {
         return setWidthConstraint(size, priority: nil, identifier: identifier)
     }
 }
