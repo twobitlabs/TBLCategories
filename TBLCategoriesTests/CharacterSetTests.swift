9419bf960812ed3cc4979bcab4b29eee48ff84a5
Christopher Pickslay <cpickslay@twobitlabs.com>
2017-05-03 14:40:38 -0700

    adds flags to emojiCharacterSet

--- TBLCategoriesTests/CharacterSetTests.swift
+++ TBLCategoriesTests/CharacterSetTests.swift
@@ -9,5 +9,9 @@ class CharacterSetTests : XCTestCase {
         expect("😀".rangeOfCharacter(from: emoji)).toNot(beNil())
         expect(emoji.contains("😀")).to(beTrue())
         expect(emoji.contains("a")).to(beFalse())
+        let england = CharacterSet(charactersIn: "🇬🇧")
+        expect(england.isSubset(of: emoji)).to(beTrue())
+        let mexico = CharacterSet(charactersIn: "🇲🇽")
+        expect(mexico.isSubset(of: emoji)).to(beTrue())
     }
 }
