eac31fa7be17dfb5ab2cadf0e6d26ba481476ccd
Todd Huss <thuss@twobitlabs.com>
2017-06-15 14:24:24 -0700

    Adds safe insert to Array

---
+++ Swift Extensions/Array+TBL.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension Array {
+    mutating func insert(_ element: Element, safeAt index: Index) {
+        var safeIndex = index
+        if index < 0 {
+            safeIndex = 0
+        } else if index > count {
+            safeIndex = count
+        }
+        insert(element, at: safeIndex)
+    }
+}
