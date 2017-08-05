e7779513044a14fcc7973e6f72c8199e2c07bf0e
Todd Huss <thuss@twobitlabs.com>
2017-08-05 14:52:46 -0700

    Adds isIpadPro12 var to UIDevice+TBL

--- Swift Extensions/UIDevice+TBL.swift
+++ Swift Extensions/UIDevice+TBL.swift
@@ -6,6 +6,10 @@ extension UIDevice {
         return userInterfaceIdiom == .pad
     }
 
+    var isIpadPro12: Bool {
+        return isIpad && (UIScreen.main.bounds.size.height == 1366 || UIScreen.main.bounds.size.width == 1366)
+    }
+
     var isIphone: Bool {
         return userInterfaceIdiom == .phone
     }
