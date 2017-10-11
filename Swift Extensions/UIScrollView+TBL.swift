fa7bfec51fbdd1052a6576c41547a408938fb8c3
Todd Huss <thuss@twobitlabs.com>
2017-10-11 12:56:54 -0700

    Update scrollToTop for iOS 11 to use adjustedContentInset

--- Swift Extensions/UIScrollView+TBL.swift
+++ Swift Extensions/UIScrollView+TBL.swift
@@ -4,7 +4,13 @@ import UIKit
 
 public extension UIScrollView {
     
-    public func scrollToTop() {
-        self.setContentOffset(CGPoint(x: 0, y: -self.contentInset.top), animated: true)
+    public func scrollToTop(animated: Bool = true) {
+        let y: CGFloat = {
+            if #available(iOS 11.0, *) {
+                return -self.adjustedContentInset.top
+            }
+            return -self.contentInset.top
+        }()
+        self.setContentOffset(CGPoint(x: 0, y: y), animated: animated)
     }
 }
