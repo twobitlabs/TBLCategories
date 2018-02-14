5b6d5b4bf59ed02fda23aa715e8108e1bef2ba05
Todd Huss <thuss@twobitlabs.com>
2018-02-14 13:24:51 -0800

    Add debugJson to collection to support `po print(dict.debugJson)`

--- Swift Extensions/Collection+TBL.swift
+++ Swift Extensions/Collection+TBL.swift
@@ -5,4 +5,14 @@ extension Collection {
     subscript (safe index: Index) -> Iterator.Element? {
         return indices.contains(index) ? self[index] : nil
     }
+
+    var debugJson: String {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: self, options: [.prettyPrinted])
+            guard let jsonString = String(data: jsonData, encoding: String.Encoding.utf8) else { return "" }
+            return jsonString
+        } catch {
+            return ""
+        }
+    }
 }
