4dac3c732d42f7d79a09ae5ea956eba985deca23
Todd Huss <thuss@twobitlabs.com>
2018-08-27 16:42:27 -0700

    Adds safer and shorter JSON parsing

---
+++ Swift Extensions/KeyedDecodingContainer+TBL.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+// From http://kean.github.io/post/codable-tips-and-tricks#41-getting-rid-of-explicit-type-parameters
+
+public struct Safe<Base: Decodable>: Decodable {
+    public let value: Base?
+
+    public init(from decoder: Decoder) throws {
+        do {
+            let container = try decoder.singleValueContainer()
+            self.value = try container.decode(Base.self)
+        } catch {
+            assertionFailure("ERROR: \(error)")
+            self.value = nil
+        }
+    }
+}
+
+extension KeyedDecodingContainer {
+    public func decode<T: Decodable>(_ key: Key, as type: T.Type = T.self) throws -> T {
+        return try self.decode(T.self, forKey: key)
+    }
+
+    public func decodeIfPresent<T: Decodable>(_ key: KeyedDecodingContainer.Key) throws -> T? {
+        return try decodeIfPresent(T.self, forKey: key)
+    }
+}
+
+extension KeyedDecodingContainer {
+    public func decodeSafely<T: Decodable>(_ key: KeyedDecodingContainer.Key) -> T? {
+        return self.decodeSafely(T.self, forKey: key)
+    }
+
+    public func decodeSafely<T: Decodable>(_ type: T.Type, forKey key: KeyedDecodingContainer.Key) -> T? {
+        let decoded = try? decode(Safe<T>.self, forKey: key)
+        return decoded?.value
+    }
+
+    public func decodeSafelyIfPresent<T: Decodable>(_ key: KeyedDecodingContainer.Key) -> T? {
+        return self.decodeSafelyIfPresent(T.self, forKey: key)
+    }
+
+    public func decodeSafelyIfPresent<T: Decodable>(_ type: T.Type, forKey key: KeyedDecodingContainer.Key) -> T? {
+        let decoded = try? decodeIfPresent(Safe<T>.self, forKey: key)
+        return decoded??.value
+    }
+
+    public func decodeSafelyArray<T: Decodable>(of type: T.Type, forKey key: KeyedDecodingContainer.Key) -> [T] {
+        let array = decodeSafely([Safe<T>].self, forKey: key)
+        return array?.compactMap { $0.value } ?? []
+    }
+}
+
+extension JSONDecoder {
+    public func decodeSafelyArray<T: Decodable>(of type: T.Type, from data: Data) -> [T] {
+        guard let array = try? decode([Safe<T>].self, from: data) else { return [] }
+        return array.compactMap { $0.value }
+    }
+}
