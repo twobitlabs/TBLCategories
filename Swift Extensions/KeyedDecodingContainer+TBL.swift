e7fa943bf3302d0c28a752d9e24b615650cba2a3
Todd Huss <thuss@twobitlabs.com>
2018-08-27 17:55:51 -0700

    Renaming decodeSafelyIfPresent to decodeOptional

--- Swift Extensions/KeyedDecodingContainer+TBL.swift
+++ Swift Extensions/KeyedDecodingContainer+TBL.swift
@@ -1,16 +1,16 @@
 import Foundation
 
-// From http://kean.github.io/post/codable-tips-and-tricks#41-getting-rid-of-explicit-type-parameters
+// Inspired by http://kean.github.io/post/codable-tips-and-tricks#41-getting-rid-of-explicit-type-parameters
 
-public struct Safe<Base: Decodable>: Decodable {
-    public let value: Base?
+struct OptionalDecodable<T: Decodable>: Decodable {
+    public let value: T?
 
     public init(from decoder: Decoder) throws {
         do {
             let container = try decoder.singleValueContainer()
-            self.value = try container.decode(Base.self)
+            self.value = try container.decode(T.self)
         } catch {
-            assertionFailure("ERROR: \(error)")
+            print("Error decoding \(String(describing: T.self)) \(error)")
             self.value = nil
         }
     }
@@ -27,38 +27,29 @@ extension KeyedDecodingContainer {
 }
 
 extension KeyedDecodingContainer {
-    public func decodeSafely<T: Decodable>(_ key: KeyedDecodingContainer.Key) -> T? {
-        return self.decodeSafely(T.self, forKey: key)
+    public func decodeOptional<T: Decodable>(_ key: KeyedDecodingContainer.Key) -> T? {
+        return self.decodeOptional(T.self, forKey: key)
     }
 
-    public func decodeSafely<T: Decodable>(_ type: T.Type, forKey key: KeyedDecodingContainer.Key) -> T? {
-        let decoded = try? decode(Safe<T>.self, forKey: key)
-        return decoded?.value
-    }
-
-    public func decodeSafelyIfPresent<T: Decodable>(_ key: KeyedDecodingContainer.Key) -> T? {
-        return self.decodeSafelyIfPresent(T.self, forKey: key)
-    }
-
-    public func decodeSafelyIfPresent<T: Decodable>(_ type: T.Type, forKey key: KeyedDecodingContainer.Key) -> T? {
-        let decoded = try? decodeIfPresent(Safe<T>.self, forKey: key)
+    public func decodeOptional<T: Decodable>(_ type: T.Type, forKey key: KeyedDecodingContainer.Key) -> T? {
+        let decoded = try? decodeIfPresent(OptionalDecodable<T>.self, forKey: key)
         return decoded??.value
     }
 
-    public func decodeSafelyArray<T: Decodable>(of type: T.Type, forKey key: KeyedDecodingContainer.Key) -> [T]? {
-        let array = decodeSafely([Safe<T>].self, forKey: key)
+    public func decodeOptionalArray<T: Decodable>(of type: T.Type, forKey key: KeyedDecodingContainer.Key) -> [T]? {
+        let array = decodeOptional([OptionalDecodable<T>].self, forKey: key)
         return array?.compactMap { $0.value }
     }
 }
 
 extension JSONDecoder {
-    public func decodeSafely<T: Decodable>(_ type: T.Type, from data: Data) -> T? {
-        let decoded = try? decode(Safe<T>.self, from: data)
+    public func decodeOptional<T: Decodable>(_ type: T.Type, from data: Data) -> T? {
+        let decoded = try? decode(OptionalDecodable<T>.self, from: data)
         return decoded?.value
     }
 
-    public func decodeSafelyArray<T: Decodable>(of type: T.Type, from data: Data) -> [T]? {
-        guard let array = try? decode([Safe<T>].self, from: data) else { return nil }
+    public func decodeOptionalArray<T: Decodable>(of type: T.Type, from data: Data) -> [T]? {
+        guard let array = try? decode([OptionalDecodable<T>].self, from: data) else { return nil }
         return array.compactMap { $0.value }
     }
 }
