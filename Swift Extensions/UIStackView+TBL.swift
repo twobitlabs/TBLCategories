a51276d728f0c94a5bf95839d1007e2fbcb09ded
Todd Huss <thuss@twobitlabs.com>
2021-01-28 13:24:54 -0800

    Fix memory leak in UIStackView removeArrangedSubviews

--- Swift Extensions/UIStackView+TBL.swift
+++ Swift Extensions/UIStackView+TBL.swift
@@ -4,5 +4,7 @@ import UIKit
 extension UIStackView {
     public func removeArrangedSubviews() {
         arrangedSubviews.forEach({ $0.removeFromSuperview() })
+        // UIStackView retains removed subviews until layoutIfNeeded is called (iOS 14)
+        layoutIfNeeded()
     }
 }
