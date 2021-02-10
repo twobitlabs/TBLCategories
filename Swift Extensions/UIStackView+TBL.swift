037eef8276f959f574156d98ea4a57f2ecb3595f
Dave Roy <droy@twobitlabs.com>
2021-02-10 10:45:11 -0800

    Only layoutIfNeeded if doing work

--- Swift Extensions/UIStackView+TBL.swift
+++ Swift Extensions/UIStackView+TBL.swift
@@ -3,8 +3,10 @@ import UIKit
 
 extension UIStackView {
     public func removeArrangedSubviews() {
-        arrangedSubviews.forEach({ $0.removeFromSuperview() })
-        // UIStackView retains removed subviews until layoutIfNeeded is called (iOS 14)
-        layoutIfNeeded()
+        if !arrangedSubviews.isEmpty {
+            arrangedSubviews.forEach({ $0.removeFromSuperview() })
+            // UIStackView retains removed subviews until layoutIfNeeded is called (iOS 14)
+            layoutIfNeeded()
+        }
     }
 }
